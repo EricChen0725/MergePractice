b8824e56019dd039774a4f3bb7d1ef2876bf2263
EricChen <eric41712@gmail.com>
2020-06-27 05:37:22 +0800

    First commit

---
+++ random_selector/data.js
@@ -0,0 +1,6 @@
+let pictures = 
+[
+    "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/06/13/99/6429046.png&x=0&y=0&sw=0&sh=0&sl=W&fw=800&exp=3600&w=465&nt=1",
+    "https://1.bp.blogspot.com/-xnkYrDm7yZ8/XirSVlXBH0I/AAAAAAAAJ7o/FTlG5YU0C7Em9AWUKOGJ0Q9FeVNxmrfegCKgBGAsYHg/s640/IMG_5152.jpg",
+    "https://www.17life.com/Images/imagesU/5/5ff76926-cd66-481e-9c48-52798c9a7ce9/%E9%BB%91%E8%B1%AC%E6%B0%B4%E9%A4%83600X600.jpg"
+];
---
+++ youtube_player/playlist.js
@@ -0,0 +1,14 @@
+//影片來源與撥放起迄時間
+//YouTube影片ID
+let playList =[
+    "IWFxKVar0BU",
+    "Xv1Yx3eFnEg",
+    "NliYy7iqh-U"
+];
+
+//播放起訖秒數
+let playTime = [
+    [5,12],
+    [10,18],
+    [15,23]
+]
